3a4ffe55d4bc95cf761798cf6b4c2bf0da15d74a
Kevin Day <kevin.r.day@gmail.com>
2016-06-10 15:34:49 -0500

    blank template useful to have around.

--- augur-on-ubuntu/configure-augur.sh
+++ augur-on-ubuntu/configure-augur.sh
@@ -20,29 +20,3 @@ echo "User home dir: $HOMEDIR"
 echo "vmname: $VMNAME"
 
 cd $HOMEDIR
-
-#####################
-# install tools
-#####################
-time sudo apt-get install -y build-essential
-time sudo apt-get -y install git
-time sudo apt-get -y install libssl-dev
-
-####################
-# Intsall Geth
-####################
-time sudo apt-get install -y software-properties-common
-time sudo add-apt-repository -y ppa:ethereum/ethereum
-time sudo add-apt-repository -y ppa:ethereum/ethereum-dev
-time sudo apt-get update
-time sudo apt-get install -y ethereum
-
-
-sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/web/augur-on-ubuntu/geth.conf
-sed -i "s/auguruser/$AZUREUSER/g" geth.conf
-
-touch /var/log/geth.sys.log
-chown $AZUREUSER /var/log/geth.sys.log
-
-date
-echo "completed augur install $$"
